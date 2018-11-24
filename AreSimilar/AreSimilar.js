5dba85d7b5c0cfe985109aae7aa115be3446731d
Vishesh <visheshsingh16@gmail.com>
2018-11-24 17:14:28 -0500

    :white_check_mark: Add Similar :heavy_plus_sign:

---
+++ AreSimilar/AreSimilar.js
@@ -0,0 +1,31 @@
+function areSimilar(a, b) {
+  if (a.length !== b.length) return false;
+
+  if (a.toString() === b.toString()) {
+    return true;
+  }
+
+  let c = [],
+    d = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      c.push(a[i]);
+      d.push(b[i]);
+    }
+  }
+  d = d.reverse();
+
+  if (c.length === 2 || c.toString() == d.toString()) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(
+  areSimilar([1, 2, 4, 3, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9])
+);
