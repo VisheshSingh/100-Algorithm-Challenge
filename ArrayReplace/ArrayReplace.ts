a314710d63e3913c63c9a83ad6573ad67ab529eb
Vishesh <visheshsingh16@gmail.com>
2018-11-25 13:11:26 -0500

    :bulb:Array Replace :hear_no_evil:

---
+++ ArrayReplace/ArrayReplace.ts
@@ -0,0 +1,15 @@
+function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  }
+  return inputArray;
+}
+
+console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1, 8, 1], 1, 3));
