56a4c02b4f144c06cdffae19907b98614a81b6b7
Vishesh <visheshsingh16@gmail.com>
2018-11-27 14:41:18 -0500

    Crossing sum:children_crossing:

---
+++ CrossingSum/CrossingSum.ts
@@ -0,0 +1,11 @@
+function crossingSum(matrix: number[][], a: number, b: number): number {
+  let colTotal = matrix[a].reduce((a, b) => a + b);
+  let rowTotal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    rowTotal += i !== a ? matrix[i][b] : 0;
+  }
+  return colTotal + rowTotal;
+}
+
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 1, 3));
