66078d3ecaff289d962a1d7db364ba7c60f7121b
Vishesh <visheshsingh16@gmail.com>
2018-11-26 20:37:56 -0500

    :bulb: Compare Integers :beers:

---
+++ CompareIntegers/compareIntegers.ts
@@ -0,0 +1,13 @@
+function compareIntegers(a: string, b: string): string {
+  if (parseInt(a) > parseInt(b)) {
+    return "greater";
+  } else if (parseInt(a) < parseInt(b)) {
+    return "lesser";
+  } else {
+    return "equal";
+  }
+}
+
+console.log(compareIntegers("12", "13"));
+console.log(compareIntegers("875", "799"));
+console.log(compareIntegers("1000", "1000"));
