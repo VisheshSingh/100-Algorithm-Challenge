d143cde9049e4edd382ccbff0b7417c22b7989a4
Vishesh <visheshsingh16@gmail.com>
2018-12-02 14:34:03 -0500

    First Digit:hash:

---
+++ FirstDigit/firstDigit.js
@@ -0,0 +1,14 @@
+function firstDigit(inputString) {
+  let digits = "1234567890".split("");
+  inputString = inputString.split("");
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (digits.includes(inputString[i])) {
+      return inputString[i];
+    }
+  }
+}
+
+console.log(firstDigit("var_1__Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit("0ss"));
