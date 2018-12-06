108de1fc33f4fad29509712ce3b419be97a617e9
Vishesh <visheshsingh16@gmail.com>
2018-12-06 15:00:57 -0500

    Pig :pig: Latin :sa:

---
+++ Piglatin/pigLatin.ts
@@ -0,0 +1,22 @@
+function pigLatin(str: string): string {
+  const letters = str.split("");
+  const vowelRegex = /[aeiou]/g;
+
+  if (vowelRegex.test(str[0])) {
+    return `${str}way`;
+  }
+
+  while (true) {
+    if (!vowelRegex.test(letters[0])) {
+      letters.push(letters.splice(0, 1)[0]);
+    } else {
+      break;
+    }
+  }
+
+  str = letters.join("") + "ay";
+  return str;
+}
+
+console.log(pigLatin("glove"));
+console.log(pigLatin("eight"));
