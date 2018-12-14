09e68c521ea172241b32770face110d8212441f2
Vishesh <visheshsingh16@gmail.com>
2018-12-14 14:57:31 -0500

    Reverse string:dolls:

---
+++ ReverseString/ReverseString.ts
@@ -0,0 +1,9 @@
+function reverseAString(str: string): string {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+console.log(reverseAString("hello"));
+console.log(reverseAString("Howdy"));
