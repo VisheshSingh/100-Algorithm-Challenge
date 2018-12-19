99fcb204217d99378c0a8887c0c592371f32da3b
Vishesh <visheshsingh16@gmail.com>
2018-12-18 19:11:28 -0500

    Valid Time :clock10:

---
+++ ValidTime/ValidTime.ts
@@ -0,0 +1,15 @@
+function validTime(time: string): boolean {
+    let [hours, minutes] = time.split(':');
+
+    if(parseInt(hours)>23 || parseInt(hours)<0){
+        return false;
+    }
+    if(parseInt(minutes)>59 || parseInt(minutes)<0){
+        return false;
+    }
+    return true;
+}
+
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
